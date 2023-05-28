7062c6689bd31a58c0a7697453930d027008d53c
Fernando Cuellar <fercuellar@gmail.com>
2023-05-28 17:15:21 -0600

    correcion de errores

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,11 +1,5 @@
 import express from 'express';
-import { 
-    createHotel, 
-    updateHotel, 
-    deleteHotel, 
-    getHotel, 
-    getHotels 
-} from '../controllers/hotels.js';
+import { createHotel } from '../controllers/hotel.js';
 import Hotel from '../models/Hotel.js';
 
 const router = express.Router();
