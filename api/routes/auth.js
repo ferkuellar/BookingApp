a62278fbf458ed9fc0670c712f07e533d7c07583
Fernando Cuellar <fercuellar@gmail.com>
2023-05-28 18:20:18 -0600

    se agregaron los archivos de auth

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import { register } from '../controllers/auth.js';
 
 const router = express.Router();
 
-router.get('/register', register);
+router.post('/register', register);
+router.post('/login', login);
 
 
 export default router;
